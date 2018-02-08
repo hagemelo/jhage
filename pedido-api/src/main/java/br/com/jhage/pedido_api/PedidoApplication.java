04d92b4d3e3c49ae3bcd63bc86067c2f7c2df550
hagemelo <hagemelo@gmail.com>
2018-02-08 10:28:43 -0200

    Tratar problema com CORS

--- pedido-api/src/main/java/br/com/jhage/pedido_api/PedidoApplication.java
+++ pedido-api/src/main/java/br/com/jhage/pedido_api/PedidoApplication.java
@@ -9,4 +9,15 @@ public class PedidoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PedidoApplication.class);
 	}
+
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurerAdapter() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/produto").allowedOrigins("http://localhost:8080");
+//			}
+//		};
+//	}
+
 }
--- pedido-api/src/main/java/br/com/jhage/pedido_api/controller/ProdutoController.java
+++ pedido-api/src/main/java/br/com/jhage/pedido_api/controller/ProdutoController.java
@@ -1,8 +1,8 @@
 package br.com.jhage.pedido_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,15 +15,15 @@ import br.com.jhage.pedido_api.repository.ProdutoRepository;
  * @since 05/02/2018
  *
  */
-
+@CrossOrigin(origins = { "http://localhost:8080" }, maxAge = 3000)
 @RestController
-@RequestMapping("/produto")
 public class ProdutoController {
 	
 	@Autowired
 	private ProdutoRepository repository;
 	
-	@GetMapping(path="/all")
+	
+	@GetMapping(path="/produto")
 	public @ResponseBody Iterable<Produto> getAllUsers() {
 		// This returns a JSON or XML with the users
 		return repository.findAll();
